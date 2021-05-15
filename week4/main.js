dd71f9e81d526ab9702934cab7be30d82bd94ffd
Nick Quast <qua16001@byui.edu>
2021-05-15 16:19:47 -0700

    wee4 updates to html and team activity

--- week4/main.js
+++ week4/main.js
@@ -6,3 +6,16 @@ input.addEventListener('blur', () => alert('blurred'), false);
 input.addEventListener('change', () => alert('changed'), false);
 
 input.addEventListener('change', () => alert('changed'), false);
+
+input.value = 'Search Here';
+
+input.addEventListener('focus', function(){
+    if (input.value==='Search Here') {
+        input.value = '' 
+    }
+}, false);
+input.addEventListener('blur', function(){
+    if(input.value === '') {
+        input.value = 'Search Here';
+    } }, false);
+
